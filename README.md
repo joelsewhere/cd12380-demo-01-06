9022b9c6ee0536a3c1ffd662f126e55267f84fcc
joelsewhere <joelsewhere@gmail.com>
2026-03-22 09:53:24 -0500

    readme and params

---
+++ README.md
@@ -0,0 +1,5 @@
+# Templating
+
+- Render a template as a task argument
+- Render a templated SQL query
+- Render a templated SQL file
--- templating.py
+++ templating.py
@@ -2,14 +2,26 @@ from airflow.sdk import dag, task, Param
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 @dag(
-        params=Param("")
-)
+    params={
+        "event_type": Param(
+            None,
+            type=['string', 'null'],
+            enum=[
+                'Workshop',
+                'Webinar',
+                'Conference',
+                'Bootcamp', 
+                'Sprint',
+                'Meetup',
+                'Symposium',
+                'Hackathon',
+                'Summit',
+                'Seminar'
+                ]
+            )
+        }
+    )
 def templating():
-
-    @task
-    def print_ds(ds):
-
-        print(ds)
 
 
     @task
@@ -26,7 +38,7 @@ def templating():
         LEFT JOIN events
         ON calendar.date = events.date
         WHERE calendar.date = '{{ ds }}'
-
+        {% if params.event_type %} AND events.event_type = '{{ params.event_type }}'{% endif %}
         """
     
     sql_op = SQLExecuteQueryOperator(
@@ -46,7 +58,7 @@ def templating():
     
     path = "{{ dag.dag_id }}/file.txt"
     
-    print_ds() >> read_file(path) >>  sql_op >> sql_file_op
+    read_file(path) >>  sql_op >> sql_file_op
 
 
 templating()
